8707f4b722d27f1140c5070478c7185d3bf95dba
Best0Ioch <trifonkozurak@gmail.com>
2025-09-20 20:01:46 +0300

    Delete autopark_pr1 directory

--- autopark_pr1/driver.py
+++
@@ -1,7 +0,0 @@
-class Driver:
-    def __init__(self, name: str, driving_experience: int):
-        self.name = name
-        self.driving_experience = driving_experience
-
-    def __str__(self):
-        return f"{self.name} (водійський стаж {self.driving_experience} років)"
